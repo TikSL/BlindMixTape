3739e865b43e69d9e240f98082ab56dc51426517
HageshiiYagi <figaro.ains@gmail.com>
2023-07-28 23:43:17 +0200

    Animations Boutons
    Attention, play et quit ne fonctionnent pas correctement( soucis mineur)

--- vue/ressources.py
+++ vue/ressources.py
@@ -19,19 +19,22 @@ lobbyWindowOption2 = pygame.image.load("assets/windows/Window_35.png")
 lobbyIconPlayers = pygame.transform.scale(pygame.image.load("assets/buttons/Button_98.png"), (111, 106))
 lobbyIconDifficulty = pygame.transform.scale(pygame.image.load("assets/buttons/Button_29.png"), (111, 106))
 
-lobbyButtonPlus = [pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Button2_50.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame2.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame3.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame4.png"), (60, 60))]
+lobbyButtonPlus = [pygame.transform.scale(pygame.image.load("assets/buttons/plus/plus_1.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/plus/plus_2.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/plus/plus_3.png"), (60, 60))]
 
-lobbyButtonMoins = [pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Button2_50.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame2.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame3.png"), (60, 60)),
-        pygame.transform.scale(pygame.image.load("assets/buttons/Bouton50/Frame4.png"), (60, 60))]
+lobbyButtonMoins = [pygame.transform.scale(pygame.image.load("assets/buttons/moins/moins_1.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/moins/moins_2.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/moins/moins_3.png"), (60, 60))]
 
-lobbyButtonBack = [pygame.transform.scale(pygame.image.load("assets/buttons/Button_50.png"), (100, 100))]
+lobbyButtonBack = [pygame.transform.scale(pygame.image.load("assets/buttons/back/back_1.png"), (100, 100)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/back/back_2.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/back/back_3.png"), (60, 60))]
 
-lobbyButtonPlay = [pygame.transform.scale(pygame.image.load("assets/buttons/Button_14.png"), (427 / 1.5, 190 / 1.5))]
+
+lobbyButtonPlay = [pygame.transform.scale(pygame.image.load("assets/buttons/play/play_1.png"), (427 / 1.5, 190 / 1.5)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/play/play_2.png"), (60, 60)),
+        pygame.transform.scale(pygame.image.load("assets/buttons/play/play_3.png"), (60, 60))]
 
 # Play
 
