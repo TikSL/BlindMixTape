116cd8bee9af28aab0ce57ec7dc617b316e801bd
HageshiiYagi <figaro.ains@gmail.com>
2023-07-31 12:37:09 +0200

    Changement apportés sur les tailles dans vignette_joueur.py

--- vue/vignette_joueur.py
+++ vue/vignette_joueur.py
@@ -16,15 +16,20 @@ class VignetteJoueur:
         self.x = x
         self.y = y
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
-        self.icon_croix = Button(images=ressources.vignetteButtonCroix, pos=(self.x + 145, self.y + 5),
-                                 text_input=" ", font=ressources.get_font(ressources.nunitoRegular, 70),
+        self.icon_croix = Button(images=ressources.vignetteButtonCroix,
+                                 pos=(self.x + (0.094 * (0.049 * ressources.screen_width)),
+                                      self.y + (0.006*ressources.screen_height)),
+                                 text_input=" ", font=ressources.get_font(ressources.nunitoRegular,
+                                                                          round(0.081 * ressources.screen_height)),
                                  base_color="White",
                                  hovering_color="#6DC300")
-        self.text = TexteModifiable(self.x, self.y + 150 + 5)
+        self.text = TexteModifiable(self.x, self.y + (0.179*ressources.screen_height))
         self.bufferPerso = random.choice(ressources.persos)
         self.personnage_image = Button(
-            images=[pygame.transform.scale(pygame.image.load(self.bufferPerso), (100, 100))], pos=(self.x+75, self.y + 80),
-            text_input=" ", font=ressources.get_font(ressources.nunitoRegular, 70),
+            images=[pygame.transform.scale(pygame.image.load(self.bufferPerso),
+                                           (0.065 * ressources.screen_width, 0.116 * ressources.screen_height))],
+            pos=(self.x + (0.049*ressources.screen_width), self.y + (0.093*ressources.screen_height)),
+            text_input=" ", font=ressources.get_font(ressources.nunitoRegular, round(0.179*ressources.screen_height)),
             base_color="White",
             hovering_color="#6DC300")
 
@@ -35,14 +40,19 @@ class VignetteJoueur:
         self.personnage_image.update(screen)
 
     def realigner(self, k):
-        self.x = 900 + (k % 3) * 170
-        self.y = 125 + (k // 3) * 200
+        self.x = 0.586 * ressources.screen_width + (k % 3) * 0.197 * ressources.screen_height
+        self.y = 0.081 * ressources.screen_width + (k // 3) * 0.231 * ressources.screen_height
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
-        self.icon_croix.setPos(pos=(self.x + 150 - 5, self.y + 5))
-        self.personnage_image.setPos(pos=(self.x+75, self.y + 80))
-        self.text.setPos(pos=(self.x, self.y + 150 + 5))
+        self.icon_croix.setPos(pos=(self.x + (0.094 * ressources.screen_width), self.y +
+                                    (0.003 * ressources.screen_height)))
+        self.personnage_image.setPos(pos=(self.x + (0.049 * ressources.screen_width), self.y +
+                                          (0.093 * ressources.screen_height)))
+        self.text.setPos(pos=(self.x, self.y + (0.179 * ressources.screen_height)))
 
     def changer_personnage(self):
-        self.bufferPerso = ressources.persos[(ressources.persos.index(self.bufferPerso) + 1)%20]
-        self.personnage_image.setImages([pygame.transform.scale(pygame.image.load(self.bufferPerso), (100, 100))])
-        self.personnage_image.setPos(pos=(self.x + 75, self.y + 80))
+        self.bufferPerso = ressources.persos[(ressources.persos.index(self.bufferPerso) + 1) % 20]
+        self.personnage_image.setImages([pygame.transform.scale(pygame.image.load(self.bufferPerso),
+                                                                (0.065 * ressources.screen_width, 0.116 *
+                                                                 ressources.screen_height))])
+        self.personnage_image.setPos(pos=(self.x + (0.049 * ressources.screen_width),
+                                          self.y + (0.093 * ressources.screen_width)))
