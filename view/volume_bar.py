665e9d4ce97cc47fc21287e4e3260b2715f0e5aa
TiksL <56410745+TikSL@users.noreply.github.com>
2023-08-31 15:06:24 +0300

    Barre de volume - Position du volume initial - Ajout aide visuelle

--- view/volume_bar.py
+++ view/volume_bar.py
@@ -5,7 +5,7 @@ import ressources
 
 
 class VolumeBar:
-    def __init__(self, x, y, width, height, min_volume=0.0, max_volume=1.0):
+    def __init__(self, x, y, width, height,volume, min_volume=0.0, max_volume=1.0):
         self.bufferVolume = None
         self.x = x
         self.y = y
@@ -13,15 +13,14 @@ class VolumeBar:
         self.height = height
         self.min_volume = min_volume
         self.max_volume = max_volume
-        self.current_volume = 0.5  # Volume initial (0.5 = 50%)
+        self.current_volume = volume
         self.is_dragging = False
 
-        # Charger les images de la barre de volume (vous pouvez les remplacer par vos images)
         self.volume_fill = pygame.transform.scale(pygame.image.load("view/assets/windows/Window_66.png"), (self.width, self.height))
         self.volume_bar = pygame.transform.scale(pygame.image.load("view/assets/windows/Window_65.png"), (self.width, self.height))
         #self.volume_icon = pygame.transform.scale(pygame.image.load("assets/buttons/Button2_25.png"), (self.height*2, self.height*2))
         self.volumeButton = button.Button(
-            images=ressources.lobbySoundOn, pos=(self.x - 32, self.y + 15),
+            images=ressources.lobbySoundOn, pos=(self.x - ressources.screen_width*0.023, self.y + ressources.screen_height*0.020),
             text_input=" ", font=ressources.get_font(ressources.nunitoRegular, 70),
             base_color="White",
             hovering_color="#6DC300"
@@ -33,7 +32,11 @@ class VolumeBar:
         volume_bar_width = int(self.current_volume * self.width)
         volume_bar_current = pygame.transform.scale(self.volume_bar.subsurface((0, 0, volume_bar_width, self.height)),
                                                     (volume_bar_width, self.height))
-        surface.blit(volume_bar_current, (self.x, self.y))  # Afficher la barre de volume actuelle
+        surface.blit(volume_bar_current, (self.x, self.y))
+        print(volume_bar_width)
+        surface.blit(pygame.transform.scale(pygame.image.load("view/assets/windows/Window_25.png"),
+                                            (ressources.screen_width*0.01, ressources.screen_height*0.045))
+                     , (self.x+volume_bar_width-ressources.screen_width*0.003, self.y-ressources.screen_height*0.007))
         self.volumeButton.update(surface)
 
     def handle_event(self, event, mousePosition, screen):
